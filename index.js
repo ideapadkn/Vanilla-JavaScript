e7caccae19e87a37f7b8f777b67a885891144539
Kamron or Камрон <kamrush6429@gmail.com>
2023-07-25 13:16:47 +0500

    this, call, apply, bind

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-// const person = {
-//   name: 'Kamron',
-//   age: 20,
-//   greet: function() {
-//     console.log('this is greet')
-//   }
-// }
-
-const person = new Object({
-  name: "Kamron",
-  age: 20,
-  greet: function () {
-    console.log("this is greet");
-  },
-});
-
-Object.prototype.sayHello = function() {
-  console.log('sayHello')
+function hello() {
+  console.log("hhhhh", this);
 }
 
-const lena = Object.create(person)
-lena.name = 'Elena'
+const person = {
+  name: "Kamron",
+  age: 20,
+  sayHello: hello,
+  sayHelloWindow: hello.bind(document),
+  logInfo: function (job, phone) {
+    console.group(`${this.name} info:`);
+    console.log(`Name is ${this.name}`);
+    console.log(`Age is ${this.age}`);
+    console.log(`Job is ${job}`);
+    console.log(`Phone is ${phone}`);
+    console.groupEnd();
+  },
+};
 
-const str = new String('I am string')
+const lena = {
+  name: "Elena",
+  age: 19,
+};
+// BINID
+person.logInfo.bind(lena, "Fronend", "42986")();
+// CALL
+person.logInfo.call(lena, "Frontend", "42412");
+// APPLY
+person.logInfo.apply(lena, ["Frontend", "3218"]); // только два параметра
+
+// =============
+
+const array = [1, 2, 3, 4, 5];
+
+// function multBy(arr, n) {
+//   return arr.map(function(i) {
+//     return i * n
+//   })
+// }
+
+Array.prototype.multBy = function (n) {
+  return this.map(function (i) {
+    return i * n;
+  });
+};
+
+console.log(array.multBy(2));
